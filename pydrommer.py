a9bde6c70a20193a3a368b2ef94cf624a9c6ce0e
mkbeh <mkbehforever@gmail.com>
2019-05-14 14:15:56 +0300

    Sorted functions into classes

--- pydrommer.py
+++ pydrommer.py
@@ -25,121 +25,115 @@ from extra import utils
 
 PORTS_NUM = 65536
 HOSTS_BLOCK_SIZE = 5          # кол-во хостов из файла
-PORTS_BLOCK_SIZE = 25     # кол-во портов которые будут одновременно браться 25
+PORTS_BLOCK_SIZE = 25         # кол-во портов которые будут одновременно браться 25
 TIMEOUT = 20
 READ_TIMEOUT = .1
 
 
-"""
-ConnectionRefusedError решить как обрабатывать
-"""
-
-
 class PortsChecker:
-    pass
+    @staticmethod
+    async def close_conn(sock):
+        sock.close()
+        await sock.wait_closed()
 
+    async def check_on_open_port(self, host, port):
+        try:
+            _, writer = await asyncio.wait_for(
+                asyncio.open_connection(host, port), timeout=.1
+            )
+        except asyncio.TimeoutError:
+            return
+        except ConnectionRefusedError:
+            # эта ошибка вылетает когда слишком много одновременных соединений открыто
+            raise
+        else:
+            await self.close_conn(writer)
+            return host, port
 
-async def close_conn(sock):
-    sock.close()
-    await sock.wait_closed()
-
-
-async def check_on_open_port(host, port):
-    try:
-        _, writer = await asyncio.wait_for(
-            asyncio.open_connection(host, port), timeout=.1
+    async def find_open_ports(self, host, ports_range):
+        print('ports range', ports_range)
+        open_ports = await asyncio.gather(
+            *(self.check_on_open_port(host, port) for port in range(*ports_range))
         )
-    except asyncio.TimeoutError:
-        return
-    except ConnectionRefusedError:
-        print('PORT', port)
-        raise
-        # return
-    else:
-        await close_conn(writer)
-        return host, port
-
-
-async def find_open_ports(host, ports_range):
-    print('ports range', ports_range)
-    open_ports = await asyncio.gather(
-        *(check_on_open_port(host, port) for port in range(*ports_range))
-    )
-    print(
-        tuple(
-            filter(
-                lambda x: x is not None, open_ports
+        print(
+            tuple(
+                filter(
+                    lambda x: x is not None, open_ports
+                )
             )
         )
-    )
 
-    return filter(
-        lambda x: x is not None, open_ports
-    )
+        return filter(
+            lambda x: x is not None, open_ports
+        )
 
+    @staticmethod
+    async def calc_blocks_num():
+        return utils.truncate(
+            PORTS_NUM / PORTS_BLOCK_SIZE
+        )
 
-async def calc_blocks_num():
-    return utils.truncate(
-        PORTS_NUM / PORTS_BLOCK_SIZE
-    )
+    async def ports_checker_handler(self, host):
+        blocks_num = await self.calc_blocks_num()
+        last_block_size = PORTS_NUM - blocks_num * PORTS_BLOCK_SIZE
 
+        rpcs = []
 
-async def ports_checker_handler(host):
-    blocks_num = await calc_blocks_num()
-    last_block_size = PORTS_NUM - blocks_num * PORTS_BLOCK_SIZE
+        for i in range(0, PORTS_NUM, PORTS_BLOCK_SIZE):
+            time.sleep(.1)
 
-    rpcs = []
+            if i != blocks_num * PORTS_BLOCK_SIZE:
+                rpcs.extend(
+                    await self.find_open_ports(host, (i, i + PORTS_BLOCK_SIZE))
+                )
+                # break
+                continue
 
-    for i in range(0, PORTS_NUM, PORTS_BLOCK_SIZE):
-        time.sleep(.1)
-
-        if i != blocks_num * PORTS_BLOCK_SIZE:
             rpcs.extend(
-                await find_open_ports(host, (i, i + PORTS_BLOCK_SIZE))
+                await self.find_open_ports(host, (i, i + last_block_size))
             )
-            # break
-            continue
 
-        rpcs.extend(
-            await find_open_ports(host, (i, i + last_block_size))
-        )
-
-    return rpcs
+        return rpcs
 
 
-async def prepare_basic_data(host, block_size):
-    try:
-        blocks_num = utils.calc_blocks_num(host, block_size)
-    except FileNotFoundError:
-        return
-    else:
-        allowable_lines_num = blocks_num * block_size
-        return allowable_lines_num
+class PyDrommer:
+    def __init__(self):
+        self.checker = PortsChecker()
+
+    @staticmethod
+    async def prepare_basic_data(host, block_size):
+        try:
+            blocks_num = utils.calc_blocks_num(host, block_size)
+        except FileNotFoundError:
+            return
+        else:
+            allowable_lines_num = blocks_num * block_size
+            return allowable_lines_num
+
+    async def ports_scanner(self, host):
+        allowable_lines_num = await self.prepare_basic_data(host, HOSTS_BLOCK_SIZE)
+
+        if not isinstance(allowable_lines_num, int):
+            res = await self.checker.ports_checker_handler(host)
+            print(res)
+            # return await RPCFinder(host).test_query()
+
+        # rpc_addrs = []
+        #
+        # for i in range(1, allowable_lines_num, BLOCK_SIZE):
+        #     hosts = _get_block(host, i, i + BLOCK_SIZE)
+        #     hosts = await asyncio.gather(
+        #         *(RPCFinder(addr).test_query() for addr in hosts)
+        #     )
+        #
+        #     rpc_addrs.extend(hosts)
+        #     time.sleep(TIMEOUT)
+        #
+        # rpc_addrs = filter(lambda x: x is not None, rpc_addrs)
+        # return (addr for addr in rpc_addrs) if genexpr else tuple(rpc_addrs)
 
 
-async def ports_scanner(host):
-    allowable_lines_num = await prepare_basic_data(host, HOSTS_BLOCK_SIZE)
-
-    if not isinstance(allowable_lines_num, int):
-        res = await ports_checker_handler(host)
-        print(res)
-        # return await RPCFinder(host).test_query()
-
-    # rpc_addrs = []
-    #
-    # for i in range(1, allowable_lines_num, BLOCK_SIZE):
-    #     hosts = _get_block(host, i, i + BLOCK_SIZE)
-    #     hosts = await asyncio.gather(
-    #         *(RPCFinder(addr).test_query() for addr in hosts)
-    #     )
-    #
-    #     rpc_addrs.extend(hosts)
-    #     time.sleep(TIMEOUT)
-    #
-    # rpc_addrs = filter(lambda x: x is not None, rpc_addrs)
-    # return (addr for addr in rpc_addrs) if genexpr else tuple(rpc_addrs)
-
-
+drommer = PyDrommer()
 # asyncio.run(check_on_open_port('46.160.199.52', 18332))
 ioloop = asyncio.get_event_loop()
-ioloop.run_until_complete(ports_scanner('46.160.199.52'))
+ioloop.run_until_complete(drommer.ports_scanner('46.160.199.52'))
