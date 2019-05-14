8a62c6c49b40db2cbb5bcdba356a2a835e33b6b5
mkbeh <mkbehforever@gmail.com>
2019-05-14 14:05:36 +0300

    Added basic implementation of scanner for checking ports on one host.

--- extra/utils.py
+++ extra/utils.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+import math
+
+
+def count_lines(filename, chunk_size=1 << 13):
+    with open(filename) as file:
+        return sum(chunk.count('\n')
+                   for chunk in iter(lambda: file.read(chunk_size), ''))
+
+
+def calc_blocks_num(file, block_size):
+    lines_num = count_lines(file)
+    blocks_num = math.ceil(lines_num / block_size)
+
+    return 1 if blocks_num == 0 else blocks_num
+
+
+def truncate(num, decimals=0):
+    multiplier = 10 ** decimals
+
+    return int(
+        int(num * multiplier) / multiplier
+    )
