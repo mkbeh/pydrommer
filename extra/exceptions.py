127e89b8d138a5382772bfff8931c9eaae35c8b7
mkbeh <mkbehforever@gmail.com>
2019-05-17 00:49:46 +0300

    refactored exceptions

--- extra/exceptions.py
+++ extra/exceptions.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
 
-class PluginsBase(Exception):
+class ArgValueTypeDefinerExc(Exception):
     def __init__(self, error_msg=''):
         Exception.__init__(self, f'{error_msg}')
 
 
-class InvalidPortsRange(PluginsBase):
+class IncorrectHosts(ArgValueTypeDefinerExc):
+    def __init__(self, hosts):
+        super().__init__(error_msg=f'Incorrect hosts {hosts}.')
+
+
+class BlocksCalculatorExc(Exception):
+    def __init__(self, error_msg=''):
+        Exception.__init__(self, f'{error_msg}')
+
+
+class InvalidPortsRange(BlocksCalculatorExc):
     def __init__(self, ports_range):
         super().__init__(error_msg=f'Invalid ports range {ports_range}.')
 
 
-class IncorrectHosts(PluginsBase):
-    def __init__(self, hosts):
-        super().__init__(error_msg=f'Incorrect hosts {hosts}.')
+
