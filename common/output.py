5798a0fb19ce6a69b2653f0d9a98ec8835f317fd
mkbeh <mkbehforever@gmail.com>
2019-05-18 22:53:56 +0300

    Made: - fixes scanner - added output skeleton

--- common/output.py
+++ common/output.py
@@ -5,5 +5,13 @@ class DataSorter:
     pass
 
 
-class Output:
-    pass
+class Output(DataSorter):
+    def __init__(self):
+        super(Output, self).__init__()
+
+    def to_file(self):
+        pass
+
+    def output(self, output_to, tmp_file):
+        print('output_to - ', output_to)
+        print('tmp file - ', tmp_file)
--- plugins/scanner.py
+++ plugins/scanner.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
-import asyncio
 import functools
 
 from common.pluginbase import AsyncPluginBase
 from common.tcpbase import TCPBase
+from common.output import Output
 
 
-class PortsChecker(AsyncPluginBase, TCPBase):
-    def __init__(self, hosts, ports='1-100', hosts_block_size=10,
-                 ports_block_size=20, timeout=.1, read_timeout=.1):
-        super().__init__(hosts, ports, hosts_block_size=hosts_block_size, ports_block_size=ports_block_size)
-        self._timeout = timeout
-        self._read_timeout = read_timeout
+class PortsChecker(AsyncPluginBase, TCPBase, Output):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._timeout = kwargs.get('timeout')
+        self._read_timeout = kwargs.get('read_timeout')
+        self._output_to = kwargs.get('output_to')
 
     async def scanner(self):
         await self.run_plugin(
             functools.partial(self.find_open_ports, timeout=self._timeout, read_timeout=self._read_timeout),
             require_ports=True
         )
-
-
-from test_data import secret
-asyncio.get_event_loop().run_until_complete(PortsChecker(secret.subnet).scanner())
-
+        self.output(self._output_to, self.tmp_file)
